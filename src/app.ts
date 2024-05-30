c16b2a3ce0ce9ec1cae2cd38568194f2d3f6983b
Berta Péter <berta.peter001@gmail.com>
2024-05-30 14:23:52 +0200

    Kész

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+class Atlo {
+    aSide?: HTMLInputElement | null;
+    bSide?: HTMLInputElement | null;
+    diagon?: HTMLInputElement | null;
+    calcButton?: HTMLButtonElement | null;
+
+    constructor() {
+        this.bindHtml();
+        this.handleEvent();
+    }
+
+    bindHtml() {
+        this.aSide = document.querySelector('#aSide')
+        this.bSide = document.querySelector('#bSide')
+        this.diagon = document.querySelector('#diagon')
+        this.calcButton = document.querySelector('#calcButton')
+    }
+
+    handleEvent() {
+        this.calcButton?.addEventListener('click', () => {
+            this.startCalc();
+        });
+    }
+
+    startCalc() {
+        const aSideInput = Number(this.aSide?.value);
+        const bSideInput = Number(this.bSide?.value);
+        const diagon = this.calcDiagon(aSideInput, bSideInput);
+        this.diagon!.value = String(diagon);
+        
+    }
+
+    calcDiagon(aSide: number, bSide: number):number {
+        return Math.sqrt(Math.pow(aSide, 2) + Math.pow(bSide, 2));
+    }
+}
+
+new Atlo();
